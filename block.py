4fca6bce65b9c6769e6f798026a7b00546320d75
ARCircle1502 <31349394+ARCircle1502@users.noreply.github.com>
2019-07-24 09:08:05 -0400

    Add files via upload

---
+++ block.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, x, y, sprite, block_type):
+        self.x = x
+        self.y = y
+        self.width = 20
+        self.height = 20
+        self.sprite = sprite
+        self.block_type = block_type
+        self.hitbox = pygame.rect.Rect((x, y), (self.width, self.height))
+
